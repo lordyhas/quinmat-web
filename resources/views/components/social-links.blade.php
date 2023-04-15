cb885e444cfb2c341c3ce15618e3b9e84829e376
lordyhas <dev.haspro@gmail.com>
2023-04-15 22:47:44 +0200

    version 0.0.6+2 | social icons

---
+++ resources/views/components/social-links.blade.php
@@ -0,0 +1,11 @@
+@php
+     $appConst = config("app.app_constant");
+@endphp
+
+<div class="social-links">
+    <a href="{{config("app.facebook")}}" class="link"><i class="ti-facebook"></i></a>
+    <a href="{{config("app.twitter")}}" class="link"><i class="ti-twitter-alt"></i></a>
+    <a href="javascript:void(0)" class="link"><i class="ti-linkedin"></i></a>
+    <a href="javascript:void(0)" class="link"><i class="ti-instagram"></i></a>
+    <a href="javascript:void(0)" class="link"><i class="ti-rss"></i></a>
+</div>
