1a2540e96877ec73a73fe6a01618ac877e783dac
lordyhas <dev.haspro@gmail.com>
2023-06-05 15:23:00 +0200

    version 0.0.13+1

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\DoctorController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use League\Csv\Reader;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -20,39 +21,37 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/csv', function () {
-
-    $csv = Reader::createFromPath(storage_path('app/data_doctor.csv'));
+    //data_doctor
+    ///doctor-sample-data1
+    $csv = Reader::createFromPath(storage_path('app/data_doctor1.csv'));
     $csv->setHeaderOffset(0);
     $records = $csv->getRecords();
     $data = array();
     foreach ($records as $record) {
-        // Do something with the record
-        // You can access the fields using the named keys
         $data[] = $record;
     }
-
-    return response()->json($data) ; //iterator_to_array($records);
+    return response()->json(["data" => $data]);
 
 });
 
 
 Route::post('/test_post', function (Request $request) {
 
-    if($request->has('host'))
-        $id = $request->get('host');
+    if ($request->has('id'))
+        $id = $request->get('id');
     else $id = "null";
 
-    return response()->json([[
+    return response()->json([
         'status' => true,
-        'message' => "Product Created successfully!",
-        'host' => $id,
-    ], 200]);
+        'message' => "Message received successfully!",
+        'id' => $id,
+    ]);
 
 });
 
 Route::controller(DoctorController::class)->group(function () {
-    Route::get('/doctor_create', 'create')->name("doctor.create");
+    Route::post('/doctor/create', 'create')->name("doctor.create");
     //Route::post('/doctor_create', 'create')->name("doctor.create");
-    Route::post('/doctor_save', 'store')->name("doctor.store");
-    Route::get('/doctor_get', 'show')->name("doctor.show");
+    Route::post('/doctor/save', 'store')->name("doctor.store");
+    Route::get('/doctor/retrieve', 'show')->name("doctor.show");
 });
